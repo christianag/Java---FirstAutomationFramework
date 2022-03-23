4d53f2c2a588281e12933966d37b2ebdc0584678
Christiana <christiana.georgieva@gmail.com>
2022-03-23 19:57:47 +0200

    Update ExcelFile.java

--- src/main/java/excel/ExcelFile.java
+++ src/main/java/excel/ExcelFile.java
@@ -7,13 +7,11 @@ public class ExcelFile {
     public String name;
     public String email;
     public String issue_date;
-//    public String mejeri_final_exam_date;
 
     public ExcelFile(String name, String email, String issue_date) {
         this.name = name;
         this.email = email;
         this.issue_date = issue_date;
-//        this.mejeri_final_exam_date = mejeri_final_exam_date;
     }
 
 
